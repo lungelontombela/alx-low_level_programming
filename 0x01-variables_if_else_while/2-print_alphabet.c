253f0ce1db058427d5823cd15b7170a32b44dad0
lungelontombela <lerato7221@gmail.com>
2023-02-18 05:15:03 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Discription: Get a rondom it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a',
+
+	while (c <=  '2')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
