74fdd3a01d1b2dc9cdea57ff26fde57d501a19d8
lungelontombela <lerato7221@gmail.com>
2023-02-19 04:06:30 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@
  * 01 and 10 are considered the same combination of the two digits 0 and 1
  * Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * putchar function (every other function (printf, puts, etc…) is forbidden)
  * You can only use putchar five times maximum in your code
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
