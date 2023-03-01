dc398ced27f4c4c3d7e77916f0daca0eeff5cafc
lungelontombela <lerato7221@gmail.com>
2023-03-01 13:52:18 -0800

    103-infinite_add.c

---
+++ 103-infinite_add.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * infinite_add - add two number
+ * @n1: input value
+ * @n2: input value
+ * @size_r: input value
+ * @r: input value
+ * Return: 0
+ */
+
+char *infinite_add(char *n1, char *n2, s char *r, int size_r)
+{
+	int overflow = 0, i = 0, digit = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+		val2 = 0;
+		else
+		val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
