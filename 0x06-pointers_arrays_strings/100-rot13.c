91441181620c832bd5e9530bc1b7a65a5148bdf0
lungelontombela <lerato7221@gmail.com>
2023-03-02 08:35:41 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,21 +9,23 @@
 
 char *rot13(char *s)
 {
-        int i;
-        int j;
-        char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	 int i;
+	 int j;
 
-        for (i = 0; s[i] != '\0'; i++)
-	{
-          for (j = 0; j < 52; j++)
-	  {
-            if (s[i] == data1[j])
-	   {
-		    s[i] = datarot[j];
-		     break;
-	  }
+	 char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	 char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-        }
- return (s);
+	 for (i = 0; s[i] != '\0'; i++)
+	 {
+		 for (j = 0; j < 52; j++)
+		 {
+			 if (s[i] == data1[j])
+			 {
+				 s[i] = datarot[j];
+				 break;
+			 }
+		 }
+	 }
+	 return (s);
 }
+
