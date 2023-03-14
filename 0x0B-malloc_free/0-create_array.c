1c79bc5596d19a1ff1271ecda05a1da2873cf602
lungelontombela <lerato7221@gmail.com>
2023-03-14 03:59:19 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
  */
-char *create_array(unsined int size, char c)
+char *create_array(unsigned int size, char c)
 
 {
 	char *str;
