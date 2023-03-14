2409c4b70295fec6b3122d9a1c9aceb3dfc2e594
lungelontombela <lerato7221@gmail.com>
2023-03-14 09:17:51 -0700

    1-strdup.c

--- 1-strdup.c
+++ 1-strdup.c
@@ -9,23 +9,28 @@
  */
 char *_strdup(char *str)
 {
-	char *duplicate;
-	unsigned int i, len;
-
-	i = 0;
-	len = 0;
+	int a = 0, i = 1;
+	char *s;
 
 	if (str == NULL)
 		return (NULL);
 
-	while (str[len])
-		len++;
-	duplicate = malloc(sizeof(char) * (len + 1));
+	while (str[i])
+	{
+		i++;
+	}
 
-	if (duplicate == NULL)
+	s = malloc((sizeof(char) * i) + 1);
+
+	if (s == NULL)
 		return (NULL);
 
-	while ((duplicate[i] = str[i]) != '\0')
-		i++;
-	return (duplicate);
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+
+	s[a] = '\0';
+	return (s);
 }
