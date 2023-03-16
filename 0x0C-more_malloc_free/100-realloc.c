d6b749fdd3e4b86b2393bdb2cf6bb1e1662a216f
lungelontombela <lerato7221@gmail.com>
2023-03-16 12:33:49 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,20 +14,23 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == 0 && ptr != NULL)
+	char *nptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if ((new_size == 0) && (ptr != NULL))
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	if (ptr == NULL)
-		ptr = mallloc(new_size);
-
-	if (new_size == old_size)
-		return (ptr);
-
-	free(ptr);
-	ptr = malloc(new_size);
-
-	return (ptr);
+	if (new_size > old_size && (ptr != NULL))
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (nptr);
+		for (i = 0; i < old_size; i++)
+			nptr[i] = *((char *)ptr + 1);
+	}
+	return (nptr);
 }
