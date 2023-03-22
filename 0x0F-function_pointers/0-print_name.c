5d695ab2dffb5ec7ba312007c9e191a91d537e2a
lungelontombela <lerato7221@gmail.com>
2023-03-22 13:22:21 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,11 +6,11 @@
  * @name: string to add
  * @f: pointer to function
  * Return: nothing
- **/
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 		return;
 
 	f(name);
